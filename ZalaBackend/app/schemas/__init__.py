7071333d12eee689902a428c683e55921d82319c
jonathanz4344 <jzmz831@gmail.com>
2025-10-24 20:01:46 -0400

    unit init

--- ZalaBackend/app/schemas/__init__.py
+++ ZalaBackend/app/schemas/__init__.py
@@ -1,3 +1,5 @@
 from .contact import ContactBase, ContactCreate, ContactUpdate, ContactPublic
 from .address import AddressBase, AddressCreate, AddressPublic
 from .property import PropertyBase, PropertyCreate, PropertyPublic
+from .user import UserBase, UserCreate, UserUpdate, UserPublic, UserPublicWithProperties, UserPublicWithLeads
+from .unit import UnitBase, UnitCreate, UnitUpdate, UnitPublic
