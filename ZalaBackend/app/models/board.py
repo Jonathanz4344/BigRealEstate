9c37a0ac51bc666805be778e7b47f947d1092a4f
Robert Huang <rh5661@rit.edu>
2025-11-10 16:52:36 -0500

    sql alchemy models

---
+++ ZalaBackend/app/models/board.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from ..db.session import Base
+
+
+class Board(Base):
+    """
+    SQLAlchemy model for Boards
+    """
+    __tablename__ = "boards"
+
+    board_id: Mapped[int] = mapped_column(primary_key=True)
+    board_name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.user_id"), nullable=True)
+
+    user: Mapped[Optional["User"]] = relationship("User", back_populates="boards")
+    board_steps: Mapped[Optional["BoardStep"]] = relationship("BoardStep", back_populates="board")
