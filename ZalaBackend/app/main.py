438c841ad01aa15c19da164734ed8909910bacff
jonathanz4344 <jzmz831@gmail.com>
2025-11-10 18:03:34 -0500

    Update main.py

--- ZalaBackend/app/main.py
+++ ZalaBackend/app/main.py
@@ -46,9 +46,9 @@ app.include_router(google_mail.router, prefix="/api", include_in_schema=True) # 
 app.include_router(campaign_emails.send_router, prefix="/api", include_in_schema=True) # Send Campaign Email
 app.include_router(campaign_emails.router, prefix="/api", include_in_schema=True)
 
-app.include_router(campaigns.router, prefix="/api", include_in_schema=True)
-app.include_router(leads.router, prefix="/api", include_in_schema=True)
-app.include_router(users.router, prefix="/api", include_in_schema=True)
+app.include_router(campaigns.router, prefix="/api", include_in_schema=False)
+app.include_router(leads.router, prefix="/api", include_in_schema=False)
+app.include_router(users.router, prefix="/api", include_in_schema=False)
 
 app.include_router(campaign_leads.router, prefix="/api", include_in_schema=False)
 app.include_router(addresses.router, prefix="/api", include_in_schema=False)
